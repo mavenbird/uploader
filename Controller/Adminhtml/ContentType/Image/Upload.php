da5dd92e5d87b85c20115246dd0ad5727bfd8a55
Harshil Patwa <harshil@mavenbird.com>
2024-06-26 00:22:53 +0000

    Initial Commit

---
+++ Controller/Adminhtml/ContentType/Image/Upload.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * Mavenbird Technologies Private Limited
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://mavenbird.com/Mavenbird-Module-License.txt
+ *
+ * =================================================================
+ *
+ * @category   Mavenbird
+ * @package    Mavenbird_OrderInformation
+ * @author     Mavenbird Team
+ * @copyright  Copyright (c) 2018-2024 Mavenbird Technologies Private Limited ( http://mavenbird.com )
+ * @license    http://mavenbird.com/Mavenbird-Module-License.txt
+ */
+
+namespace Mavenbird\Uploader\Controller\Adminhtml\ContentType\Image;
+
+use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Filesystem;
+
+class Upload extends \Magento\Backend\App\Action implements HttpPostActionInterface
+{
+    public const UPLOAD_DIR = 'wysiwyg';
+
+    public const ADMIN_RESOURCE = 'Magento_Backend::content';
+
+    /**
+     * @var DirectoryList
+     */
+    private $directoryList;
+
+    /**
+     * @var \Magento\Framework\Controller\Result\JsonFactory
+     */
+    private $resultJsonFactory;
+
+    /**
+     * @var \Magento\Framework\File\UploaderFactory
+     */
+    private $uploaderFactory;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @var \Magento\Cms\Helper\Wysiwyg\Images
+     */
+    private $cmsWysiwygImages;
+
+    /**
+     * @var \Magento\Framework\Filesystem\Directory\WriteInterface
+     */
+    private $mediaDirectory;
+
+    /**
+     * Upload constructor.
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\File\UploaderFactory $uploaderFactory
+     * @param DirectoryList $directoryList
+     * @param \Magento\Cms\Helper\Wysiwyg\Images $cmsWysiwygImages
+     * @param Filesystem|null $filesystem
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\File\UploaderFactory $uploaderFactory,
+        DirectoryList $directoryList,
+        \Magento\Cms\Helper\Wysiwyg\Images $cmsWysiwygImages,
+        Filesystem $filesystem = null
+    ) {
+        $this->resultJsonFactory = $resultJsonFactory;
+        $this->storeManager = $storeManager;
+        $this->uploaderFactory = $uploaderFactory;
+        $this->directoryList = $directoryList;
+        $this->cmsWysiwygImages = $cmsWysiwygImages;
+        $filesystem = $filesystem ?? ObjectManager::getInstance()->create(Filesystem::class);
+        $this->mediaDirectory = $filesystem->getDirectoryWrite(DirectoryList::MEDIA);
+        parent::__construct($context);
+    }
+
+    /**
+     * Get File Path
+     *
+     * @param string $path
+     * @param string $imageName
+     * @return string
+     */
+    private function getFilePath($path, $imageName)
+    {
+        return rtrim($path, '/') . '/' . ltrim($imageName, '/');
+    }
+
+    /**
+     * Allowed Extension
+     *
+     * @return \Magento\Framework\Controller\Result\Json
+     */
+    public function execute()
+    {
+        $fieldName = $this->getRequest()->getParam('param_name');
+        $fileUploader = $this->uploaderFactory->create(['fileId' => $fieldName]);
+
+        // Set our parameters
+        $fileUploader->setFilesDispersion(false);
+        $fileUploader->setAllowRenameFiles(true);
+        $fileUploader->setAllowedExtensions(['jpeg','jpg','png','gif','svg','pdf','webp']);
+        $fileUploader->setAllowCreateFolders(true);
+
+        try {
+            if (!$fileUploader->checkMimeType(['image/png', 'image/jpeg', 'image/gif','image/webp','image/svg+xml','application/pdf'])) {
+                throw new \Magento\Framework\Exception\LocalizedException(__('File validation failed.'));
+            }
+
+            $result = $fileUploader->save($this->getUploadDir());
+            $baseUrl = $this->_backendUrl->getBaseUrl(['_type' => \Magento\Framework\UrlInterface::URL_TYPE_MEDIA]);
+            $result['id'] = $this->cmsWysiwygImages->idEncode($result['file']);
+            $result['url'] = $baseUrl . $this->getFilePath(self::UPLOAD_DIR, $result['file']);
+        } catch (\Exception $e) {
+            $result = [
+                'error' => $e->getMessage(),
+                'errorcode' => $e->getCode()
+            ];
+        }
+        return $this->resultJsonFactory->create()->setData($result);
+    }
+
+    /**
+     * GetUploadDir
+     *
+     * @return string
+     */
+    private function getUploadDir()
+    {
+        return $this->mediaDirectory->getAbsolutePath(self::UPLOAD_DIR);
+    }
+}
---
+++ view/base/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Ui/js/form/element/file-uploader':
+                'Mavenbird_Uploader/js/form/element/file-uploader'
+        }
+    }
+};
